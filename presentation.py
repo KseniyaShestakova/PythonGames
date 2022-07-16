7dbe7f45283906aa0c708f9da44492ee6e627928
KseniyaShestakova <91065721+KseniyaShestakova@users.noreply.github.com>
2022-07-16 23:17:02 +0300

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -1,5 +1,7 @@
 import sys
 
+import pygame
+
 from internal_logics import *
 
 
@@ -10,6 +12,11 @@ def print_text_on_rect(screen, text, rect, font, color, rect_color):
 
 
 class Representor:
+    screen = None
+    background_color = (204, 204, 255)
+    color_spectrum = None
+    border_color = (96, 96, 96)
+    border_width = 2
     bottle_list = []
     num_of_bottles = 0
     columns = 0
@@ -23,9 +30,10 @@ class Representor:
     num_of_layers = 0
     layer_height = 0
 
-    rects_to_bottles = dict()
+    left = 0
+    top = 0
 
-    def reset(self, _width, _height, bottle_set=BottleSet()):
+    def reset(self, bottle_set=BottleSet(), _width=600, _height=600):
         self.width = _width
         self.height = _height
         self.bottle_list = bottle_set.get_list()
@@ -42,37 +50,34 @@ class Representor:
 
         self.layer_height = (self.bottle_height // (4 * self.num_of_layers)) * 4
 
-        first_portion = self.width // (2 * self.bottle_width)
-        left = self.bottle_width
-        top = 2 * self.bottle_width
+        self.left = self.bottle_width
+        self.top = 2 * self.bottle_width
 
-        for cnt in range(first_portion):
-            self.rects_to_bottles[cnt] = pygame.Rect(left + 2 * self.bottle_width * cnt, top,
-                                                     self.bottle_width, self.bottle_height)
+    def __init__(self, _screen, _width=600, _height=600, _color_spectrum=((0, 0, 205), (255, 140, 0), (135, 206, 235),
+                                                                  (0, 255, 255), (250, 128, 114),
+                                                                  (154, 205, 50), (139, 0, 139), (255, 215, 0)),
+                 _border_color=(96, 96, 96), _border_width=2,
+                 _background_color=(204, 204, 255), bottle_set=BottleSet()):
+        self.screen = _screen
+        self.background_color = _background_color
+        self.border_color = _border_color
+        self.bottle_width = _border_width
+        self.color_spectrum = _color_spectrum
+        self.reset(bottle_set, _width, _height)
 
-        top += self.bottle_height + self.bottle_width
-
-        for cnt in range(first_portion, self.num_of_bottles):
-            self.rects_to_bottles[cnt] = pygame.Rect(left + 2 * self.bottle_width * (cnt - first_portion), top,
-                                                     self.bottle_width, self.bottle_height)
-
-    def __init__(self, _width, _height, bottle_set=BottleSet()):
-        self.reset(_width, _height, bottle_set)
-
-    def draw_bottle(self, surf, bottle, left, top, color_spectrum, background_color,
-                    border_color, border_width):
+    def draw_bottle(self, surf, bottle, left, top):
         colors = bottle.colors
 
         for layer in range(self.num_of_layers - 1):
-            curr_color = background_color
+            curr_color = self.background_color
             if colors[layer] is not None:
-                curr_color = color_spectrum[colors[layer]]
+                curr_color = self.color_spectrum[colors[layer]]
             pygame.draw.rect(surf, curr_color, pygame.Rect(left, top + layer * self.layer_height,
                                                            self.bottle_width, self.layer_height))
 
-        curr_color = background_color
+        curr_color = self.background_color
         if colors[self.num_of_layers - 1] is not None:
-            curr_color = color_spectrum[colors[self.num_of_layers - 1]]
+            curr_color = self.color_spectrum[colors[self.num_of_layers - 1]]
 
         pygame.draw.rect(surf, curr_color, pygame.Rect(left, top + (self.num_of_layers - 1) * self.layer_height,
                                                        self.bottle_width, self.layer_height // 4))
@@ -83,47 +88,46 @@ class Representor:
                          border_top_right_radius=-1, border_bottom_left_radius=self.bottle_width // 2,
                          border_bottom_right_radius=self.bottle_width // 2)
 
-        pygame.draw.rect(surf, border_color, pygame.Rect(left - border_width, top - 2 * border_width,
-                                                         border_width,
-                                                         int(self.layer_height * (
-                                                                 self.num_of_layers - (3 / 4))) + 2 * border_width))
-        pygame.draw.rect(surf, border_color, pygame.Rect(left + self.bottle_width, top - 2 * border_width,
-                                                         border_width,
-                                                         int(self.layer_height * (
-                                                                 self.num_of_layers - (3 / 4))) + 2 * border_width))
+        pygame.draw.rect(surf, self.border_color, pygame.Rect(left - self.border_width, top - 2 * self.border_width,
+                                                              self.border_width,
+                                                              int(self.layer_height * (
+                                                                      self.num_of_layers - (
+                                                                          3 / 4))) + 2 * self.border_width))
+        pygame.draw.rect(surf, self.border_color, pygame.Rect(left + self.bottle_width, top - 2 * self.border_width,
+                                                              self.border_width,
+                                                              int(self.layer_height * (
+                                                                      self.num_of_layers - (
+                                                                          3 / 4))) + 2 * self.border_width))
 
-        pygame.draw.rect(surf, border_color,
-                         pygame.Rect(left - border_width, top + int(self.layer_height * (self.num_of_layers - (3 / 4))),
-                                     self.bottle_width + 2 * border_width, 3 * (self.layer_height // 4)),
-                         width=border_width, border_radius=0, border_top_left_radius=-1,
+        pygame.draw.rect(surf, self.border_color,
+                         pygame.Rect(left - self.border_width, top + int(self.layer_height * (self.num_of_layers - (3 / 4))),
+                                     self.bottle_width + 2 * self.border_width, 3 * (self.layer_height // 4)),
+                         width=self.border_width, border_radius=0, border_top_left_radius=-1,
                          border_top_right_radius=-1, border_bottom_left_radius=self.bottle_width // 2,
                          border_bottom_right_radius=self.bottle_width // 2)
         pygame.draw.rect(surf, curr_color,
                          pygame.Rect(left, top + int(self.layer_height * (self.num_of_layers - (3 / 4))),
-                                     self.bottle_width, border_width))
+                                     self.bottle_width, self.border_width))
 
-    def draw_bottle_set(self, surf, color_spectrum, bottle_set=BottleSet(),
-                        border_width=2, border_color=(96, 96, 96), background_color=(204, 204, 255)):
-        left = self.bottle_width
-        top = 2 * self.bottle_width
-
+    def draw_bottle_set(self, surf):
         first_portion = self.width // (2 * self.bottle_width)
 
         for cnt in range(first_portion):
-            self.draw_bottle(surf, self.bottle_list[cnt], left + 2 * self.bottle_width * cnt, top,
-                             color_spectrum, background_color, border_color, border_width)
+            self.draw_bottle(surf, self.bottle_list[cnt], self.left + 2 * self.bottle_width * cnt, self.top)
 
-        top += self.bottle_height + self.bottle_width
+        self.top += self.bottle_height + self.bottle_width
 
         for cnt in range(first_portion, self.num_of_bottles):
-            self.draw_bottle(surf, self.bottle_list[cnt], left + 2 * self.bottle_width * (cnt - first_portion), top,
-                             color_spectrum, background_color, border_color, border_width)
+            self.draw_bottle(surf, self.bottle_list[cnt], self.left + 2 * self.bottle_width * (cnt - first_portion),
+                             self.top)
 
-    def start_screen(self, screen):
+        self.top -= self.bottle_height + self.bottle_width
+
+    def start_screen(self):
         clock = pygame.time.Clock()
         # asks to enter your name and returns it
         background_color = (204, 204, 255)
-        screen.fill(background_color)
+        self.screen.fill(background_color)
         base_font = pygame.font.Font(None, 32)
         comment_font = pygame.font.Font(None, 16)
 
@@ -133,9 +137,9 @@ class Representor:
 
         text_color = (255, 255, 255)
         color = (135, 206, 250)
-        print_text_on_rect(screen, 'Hello!', hello_rect, base_font, text_color, color)
-        print_text_on_rect(screen, 'Enter your name:', information_rect, base_font, text_color, color)
-        print_text_on_rect(screen, 'It is needed for saving your progress. Name should be up to 8 characters',
+        print_text_on_rect(self.screen, 'Hello!', hello_rect, base_font, text_color, color)
+        print_text_on_rect(self.screen, 'Enter your name:', information_rect, base_font, text_color, color)
+        print_text_on_rect(self.screen, 'It is needed for saving your progress. Name should be up to 8 characters',
                            comments_rect, comment_font, text_color, color)
 
         pygame.display.flip()
@@ -145,15 +149,14 @@ class Representor:
         input_color = (255, 255, 102)
         input_text = ''
 
-        pygame.draw.rect(screen, input_color, input_rect)
+        pygame.draw.rect(self.screen, input_color, input_rect)
 
         active = False
 
         confirm_rect = pygame.Rect(10, 140, 200, 32)
-        print_text_on_rect(screen, 'Confirm', confirm_rect, base_font, text_color, color)
-        print_text_on_rect(screen, input_text, input_rect, base_font, text_color, (153, 51, 255))
+        print_text_on_rect(self.screen, 'Confirm', confirm_rect, base_font, text_color, color)
+        print_text_on_rect(self.screen, input_text, input_rect, base_font, text_color, (153, 51, 255))
         pygame.display.flip()
-        confirmed = False
 
         while True:
             for event in pygame.event.get():
@@ -168,8 +171,8 @@ class Representor:
                     else:
                         active = False
                     if confirm_rect.collidepoint(pygame.mouse.get_pos()):
-                        pygame.quit()
-                        sys.exit()
+                        print("confirmed")
+                        return input_text
 
                 if not active:
                     continue
@@ -180,7 +183,49 @@ class Representor:
                     else:
                         input_text += event.unicode
 
-                print_text_on_rect(screen, input_text, input_rect, base_font, text_color, (153, 51, 255))
+                print_text_on_rect(self.screen, input_text, input_rect, base_font, text_color, (153, 51, 255))
                 pygame.display.flip()
 
                 clock.tick(60)
+
+    def collide_with(self, pos):
+        x = pos[0]
+        y = pos[1]
+        # return the number of bottle to which this point belongs
+        if x < self.left or y < self.top:
+            return None
+
+        left_offset = (x - self.left) // self.width
+        if left_offset % 2 == 1:
+            return None
+
+        left_offset //= 2
+
+        if y - self.top <= self.bottle_height:
+            return left_offset
+
+        y -= (self.top + self.bottle_height)
+
+        if y < self.bottle_width or y > self.bottle_width + self.bottle_height:
+            return None
+
+        return left_offset + self.width // (2 * self.bottle_width)
+
+    def lift(self, num):
+        # lifts the bottle with number num
+        if num < self.width // (2 * self.bottle_width):
+            x = self.top + self.bottle_width * 2 * num - self.border_width
+            y = self.left - 2 * self.border_width
+        else:
+            x = self.top + self.bottle_width * 2 * (num - self.width // (2 * self.bottle_width)) - self.border_width
+            y = self.top + self.bottle_width + self.bottle_height - 2 * self.border_width
+
+        pygame.draw.rect(self.screen, self.background_color, pygame.Rect(x, y,
+                                                                         self.bottle_width + 2 * self.border_width,
+                                                                         self.bottle_height + 3 * self.border_width))
+        self.draw_bottle(self.screen, self.bottle_list[num], x, y - self.bottle_width // 2)
+
+    def show_curr(self):
+        self.screen.fill(self.background_color)
+        self.draw_bottle_set(self.screen)
+        pygame.display.flip()
